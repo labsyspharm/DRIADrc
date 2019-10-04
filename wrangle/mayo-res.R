5814f0dc88c11c924a38e4dd1a036dc36af684fa
Artem Sokolov <artem.sokolov@gmail.com>
2019-10-04 13:08:44 -0400

    Wrangling of MAYO results from old BTR runs (#8)

---
+++ wrangle/mayo-res.R
@@ -0,0 +1,51 @@
+## Wrangles MAYO results from old BTR runs
+##
+## by Artem Sokolov
+
+library( tidyverse )
+
+synapser::synLogin()
+
+## Loader
+syn <- synExtra::synDownloader( "~/data/DRIAD/BTR", ifcollision="overwrite.local" )
+syn_csv <- function( synid ) { syn(synid) %>% read_csv(col_types = cols()) }
+
+indexDGE <- function()
+{
+    structure(list(Dataset = c("MAYO", "MAYO", "MAYO", "MAYO", "MAYO", "MAYO"),
+                   Task = c("AB", "AB", "AC", "AC", "BC", "BC"),
+                   id = c("syn18201042", "syn18143076", "syn18201045",
+                          "syn18143082", "syn18201026", "syn18143046"),
+                   Plate = c("DGE1", "DGE2", "DGE1", "DGE2", "DGE1", "DGE2")),
+              class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -6L))
+}
+
+indexBK <- function()
+{
+    structure(list(Dataset = c("MAYO", "MAYO", "MAYO"),
+                   Task = c("BC", "AB", "AC"),
+                   id = c("syn15572112", "syn15570670", "syn15572002")),
+              row.names = c(NA, -3L), class = c("tbl_df", "tbl", "data.frame"))
+}
+
+main <- function()
+{
+    ## Pull the background data off Synapse
+    BK0 <- indexBK() %>% mutate( BK = map(id, syn_csv), id=NULL )
+
+    ## Wrange background values
+    BK <- BK0 %>% mutate_at( "BK", map, summarize_all, list ) %>% unnest() %>%
+        gather( "Size", "BK", `10`:`1000` ) %>% mutate_at( "Size", as.integer )
+
+    ## Load DGE results
+    X0 <- indexDGE() %>% mutate( Results = map(id, syn_csv), id=NULL )
+
+    ## Combine everything into a single data frame
+    s <- rlang::exprs( Drug=description, nFeats=intersect, AUC )
+    mayoRes <- X0 %>% mutate_at( "Results", map, select, !!!s ) %>% unnest() %>%
+        mutate( Size=round(nFeats/10)*10 ) %>%
+        inner_join(BK, c("Dataset", "Task", "Size")) %>%
+        mutate( pval=map2_dbl(AUC, BK, ~mean(.y>=.x)), Size=NULL )
+
+    save( mayoRes, file=stringr::str_c("MAYO-",Sys.Date(),".RData") )
+}
