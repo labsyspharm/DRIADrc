564c4ab9a6f7397bcb91a67d0088a032f732e610
Artem Sokolov <artem.sokolov@gmail.com>
2019-10-11 09:55:28 -0400

    Updated FigS1

--- FigS1/FigS1.R
+++ FigS1/FigS1.R
@@ -7,7 +7,7 @@ source( "../plot.R" )
 
 FigS1 <- function()
 {
-    load( syn("syn20835686") )
+    load( syn("syn20948620") )
     load( syn("syn20928450") )
 
     v <- c( AB = "A-vs-B", AC = "A-vs-C", BC = "B-vs-C" )
@@ -22,26 +22,26 @@ FigS1 <- function()
 
     f <- function( p, k, a, step=0.5 )
     {
-        p + geom_ribbon( aes(ymin=AUC+k*s, ymax=AUC+(k+step)*s), alpha=a ) +
-            geom_ribbon( aes(ymin=AUC-k*s, ymax=AUC-(k+step)*s), alpha=a )
+        p + geom_ribbon( aes(ymin=AUC+k*s, ymax=AUC+(k+step)*s, fill=Dataset), alpha=a ) +
+            geom_ribbon( aes(ymin=AUC-k*s, ymax=AUC-(k+step)*s, fill=Dataset), alpha=a )
     }
     
     pl <- ggplot( BK, aes(x=nFeats) ) + theme_bw() + theme_bold() +
-        facet_wrap( ~Task ) + geom_line( aes(y=AUC, color=Dataset) ) +
-##        geom_text( aes(label=Task), data=TXT, color="black", fontface="bold",
-##                  size=5, x=log10(300), y=0.45, hjust=1, vjust=0.5 ) +
+        facet_wrap( ~Task ) +
+        geom_line( aes(y=AUC, color=Dataset), lwd=1.5 ) +
+##        geom_smooth( aes(y=AUC, color=Dataset), se=FALSE ) +
+        geom_text( aes(label=Task), data=TXT, color="black", fontface="bold",
+                  size=5, x=log10(15), y=0.9, hjust=0.25, vjust=0.5 ) +
         scale_color_manual( values=dsPal() ) +
+        scale_fill_manual( values=dsPal(), guide=FALSE ) +
         theme( strip.background = element_blank(), strip.text = element_blank() ) +
         scale_x_log10(name="Number of genes in set")
 
-    nSD <- seq(0,2.5,by=0.5)
-    va <- c(0.4, 0.3, 0.2, 0.1, 0.05, 0.025)
-    pl <- reduce2( nSD, va, f, .init=pl )
+    ## Uncomment this if reviewers complain about lack of confidence regions
+    ## nSD <- seq(0,.5,by=0.5)
+    ## va <- c(0.1, 0.0.5)
+    ## reduce2( nSD, va, f, .init=pl )
 
-    Y <- mayoRes %>% select( -AUC, AUC=BK ) %>% unnest()
-    ggplot( Y, aes(x=nFeats, y=AUC) ) + geom_smooth(se=FALSE) +
-        facet_wrap( ~Task )
-    
-#        ggsave( str_c("FigS1-",Sys.Date(),".pdf"), width=9, height=3 ) +
-#        ggsave( str_c("FigS1-",Sys.Date(),".png"), width=9, height=3 )
+    pl + ggsave( str_c("FigS1-",Sys.Date(),".pdf"), width=9, height=3 ) +
+        ggsave( str_c("FigS1-",Sys.Date(),".png"), width=9, height=3 )
 }
