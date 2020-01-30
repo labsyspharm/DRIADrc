40c05952b6dfcfa542d2ded62c27f77abe823ab9
Artem Sokolov <artem.sokolov@gmail.com>
2020-01-30 10:49:14 -0500

    Table 2 update

--- figures/Table2.R
+++ figures/Table2.R
@@ -1,14 +1,12 @@
 library(tidyverse)
 library(here)
 
-synapser::synLogin()
-syn <- synExtra::synDownloader("~/data/DRIAD/mech")
-
 wd <- here("mechanism", "polypharmacology")
 
 targets <- read_rds(file.path(wd, "cotarget_significance.rds")) %>%
     arrange(padj) %>% filter(Target_Class == "cotarget") %>%
     select( symbol = Symbol, direction = Class, n, p, padj )
 
+write_csv( head(targets,10), "Table2.csv" )
 write_csv( targets, "TableS1.csv" )
 
