b748723a801891b613ce88a8e9f3d0454d510c44
Artem Sokolov <artem.sokolov@gmail.com>
2020-09-10 18:28:06 -0400

    Finalized rebuttal figure 4-6

--- analyses/rebuttal/4-6.R
+++ analyses/rebuttal/4-6.R
@@ -15,9 +15,8 @@ dfx1 <- syn( "syn18145776" ) %>% read_csv2
 dfx2 <- syn( "syn17167348" ) %>% read_csv2
 
 ## Count the number of differentially expressed genes for each drug
-##nn1 <- dfx1 %>% group_by(Drug) %>% filter( FDR < 0.05 ) %>% summarize( nn = n() ) %>% arrange( desc(nn) )
-##nn2 <- dfx2 %>% group_by(Drug) %>% filter( FDR < 0.05 ) %>% summarize( nn = n() ) %>% arrange( desc(nn) )
-##bind_rows(nn1, nn2) %>% ggplot( aes(x=nn) ) + geom_histogram(binwidth=100) + ggsave( "test.pdf" )
+nn1 <- dfx1 %>% group_by(Drug) %>% filter( FDR < 0.05 ) %>% summarize( nn = n() ) %>% arrange( desc(nn) )
+nn2 <- dfx2 %>% group_by(Drug) %>% filter( FDR < 0.05 ) %>% summarize( nn = n() ) %>% arrange( desc(nn) )
 
 ## Identify drugs with a large number of differentially expressed genes
 d1 <- dfx1 %>% group_by(Drug) %>% filter( FDR < 0.05 ) %>% filter( n() > 300 ) %>% pull(Drug) %>% unique
@@ -37,21 +36,42 @@ DFX <- map2( DFX1, DFX2, c )
 ## Evalute all gene sets
 ##res <- imap( DFX, ~{cat("Evaluating ",.y,"\n"); evalGeneSets(.x, XY, lP)} )
 ##saveRDS( res,"driad-geq300.rds" )
-res <- readRDS("driad-geq1000.rds" )
+res <- readRDS("driad-geq300.rds" )
 
 RR <- bind_rows( res, .id="nn" ) %>% mutate( nFeats = map_int(Feats, length) ) %>%
     select( nn, nFeats, Drug=Set, AUC ) %>% filter( nFeats %% 100 == 0 ) %>%
-    arrange(Drug) %>% group_by( nn ) %>% summarize_at("AUC",list)
+    arrange(Drug) %>% group_by( nn, nFeats ) %>% summarize_at( "AUC", list )
+
 CM <- expand_grid(
     rename_all( RR, str_c, "x" ),
     rename_all( RR, str_c, "y" )) %>%
     mutate(cr = map2_dbl(AUCx, AUCy, cor, method="sp"),
-           lbl = as.character(round(cr,2)))
+           lbl = ifelse( nFeatsx == 300 | nFeatsy == 300,
+                        as.character(round(cr,2)), "" ) )
 
+## Plotting elements
 pal <- rev(RColorBrewer::brewer.pal(n=7, name="RdBu"))
-ggplot( CM, aes(x=nnx, y=nny, fill=cr) ) +
-    geom_tile() + theme_bw() +
-    geom_text(aes(label=lbl)) +
-    scale_fill_gradientn( colors=pal, limits=c(-1,1) ) +
-    ggsave("cr.pdf")
 
+## Panel A
+pA <- ggplot( bind_rows(nn1, nn2), aes(x=nn) ) + theme_bw() +
+    geom_histogram(binwidth=100, fill="steelblue") +
+    geom_vline(xintercept=300) +
+    scale_x_continuous( breaks=c(0, 300, 1000, 2000, 3000) ) +
+    xlab( "Number of diff. expressed genes with FDR < 0.05" ) +
+    ylab( "Number of drugs" )
+
+## Panel B
+pB <- ggplot( CM, aes(x=as.character(nFeatsx),
+                      y=as.character(nFeatsy), fill=cr) ) +
+    geom_tile() + theme_bw() +
+    geom_text( aes(label=lbl) ) +
+    scale_fill_gradientn(colors=pal, limits=c(-1,1),
+                         name="Spearman\ncorrelation") +
+    xlab( "Number of top diff. expressed genes" ) +
+    ylab( "Number of top diff. expressed genes" )
+
+## Generate a composite figure
+ff <- cowplot::plot_grid(pA, NULL, pB, nrow=1, rel_widths=c(1,0.1,1.25),
+                         labels=c("a","b",""), label_size=24 )
+ggsave( "R4-6.pdf", ff, width=9.5, height=4 )
+ggsave( "R4-6.png", ff, width=9.5, height=4 )
