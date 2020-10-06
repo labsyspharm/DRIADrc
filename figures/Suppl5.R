7aed0dd8c424bdb537ec396fa58c8c36069888aa
Artem Sokolov <artem.sokolov@gmail.com>
2020-10-06 10:56:31 -0400

    Figure S5

--- figures/Suppl5.R
+++ figures/Suppl5.R
@@ -1,3 +1,7 @@
+## Relative performance of machine learning methods
+##
+## by Artem Sokolov
+
 library( tidyverse )
 library( here )
 
@@ -9,8 +13,12 @@ R <- readRDS( here("results/allpred.rds") ) %>%
                          nn  = "Neural\nNetwork") ) %>%
     mutate( Mtd = factor(Mtd, unique(Mtd)) )
 
-ggplot( R, aes(x=Mtd, y=AUC) ) + theme_bw() +
+gg <- ggplot( R, aes(x=Mtd, y=AUC) ) + theme_bw() +
     geom_boxplot() + xlab("") +
     theme(panel.grid.major.x = element_blank(),
-          panel.grid.minor.x = element_blank()) +
-    ggsave( "test.pdf" )
+          panel.grid.minor.x = element_blank())
+
+## Compose the filename or extract it from the command line
+cmd <- commandArgs( trailingOnly=TRUE )
+fnOut <- `if`( length(cmd) > 0, cmd[1], str_c("Suppl5-", Sys.Date(), ".pdf") )
+ggsave( fnOut, gg, width=6, height=4 )
